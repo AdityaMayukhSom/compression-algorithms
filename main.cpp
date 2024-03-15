beeb93dd98fc8f7d1f1b269e5136a6eb9edc7790
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-15 22:14:55 +0530

    added main method

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "./src/algorithms/lzw.cpp"
+#include "./src/algorithms/run_length_encoding.cpp"
+#include "./src/utils/setup_utils.cpp"
+#include "./src/utils/tokenizer.cpp"
+
+int main(int argc, char* argv[]) {
+    std::string default_test_file_name = "test.txt";
+    if (argc == 2) {
+        default_test_file_name = argv[1];
+    }
+
+    std::vector<std::string> tokens = Tokenizer::getTokens(default_test_file_name, ".");
+
+    if (tokens.size() != 2) {
+        std::cerr << "USE: <executable_name> <filename>.<extension>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    SetupUtils::createDirectory("original");
+    SetupUtils::createDirectory("compressed");
+    SetupUtils::createDirectory("decompressed");
+
+    std::string input_file_path = "./original/" + default_test_file_name;
+    std::string compressed_file_path = "./compressed/" + tokens[0] + "_compressed.dev";
+    std::string decompressed_file_path = "./decompressed/" + tokens[0] + "_decompressed." + tokens[1];
+
+    Compressor* rlec = new RunLengthEncodingCompressor();
+    Compressor* lzwc = new LZWCompressor();
+
+    Compressor* com = lzwc;
+
+    com->compress(input_file_path, compressed_file_path);
+    com->decompress(compressed_file_path, decompressed_file_path);
+}
