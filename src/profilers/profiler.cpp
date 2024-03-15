476c27695a80d5918f50d3ed9b7717eae6a88c4a
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-16 00:08:21 +0530

    fix parameter names

--- src/profilers/profiler.cpp
+++ src/profilers/profiler.cpp
@@ -2,6 +2,6 @@
 class Profiler {
    public:
     virtual float calculate_space_saving() const = 0;
-    virtual float calculate_compression_ratio(const std::string input_file_path,
-                                              const std::string output_file_path) const = 0;
+    virtual float calculate_compression_ratio(const std::string original_file_path,
+                                              const std::string compressed_file_path) const = 0;
 };
