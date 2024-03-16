eec9d329778ffb9dd2bc3e0cc02781e618167ccc
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-16 22:26:44 +0530

    added processing speed support

--- src/profilers/profiler.cpp
+++ src/profilers/profiler.cpp
@@ -1,11 +1,15 @@
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
 class Profiler {
    public:
-    static double calculate_space_saving(double compression_ratio) { return (1 - (1 / compression_ratio)) * 100; }
-    static double calculate_compression_ratio(const std::string original_file_path,
-                                              const std::string compressed_file_path) {
+    static double calculate_space_saving(double compression_ratio) {
+        return (1 - (1 / compression_ratio)) * 100;
+    }
+    static double calculate_compression_ratio(
+        const std::string original_file_path,
+        const std::string compressed_file_path) {
         std::ifstream original_file(original_file_path, std::ios::binary);
         std::ifstream compressed_file(compressed_file_path, std::ios::binary);
 
@@ -28,10 +32,36 @@ class Profiler {
             return -1;
         }
 
-        double compression_ratio = original_file_size / (double)compressed_file_size;
+        double compression_ratio =
+            original_file_size / (double)compressed_file_size;
 
         original_file.close();
         compressed_file.close();
         return compression_ratio;
+    }
+
+    static double calculate_processing_speed(const clock_t start,
+                                             const clock_t end,
+                                             const std::string filename,
+                                             const std::string unit = "MB") {
+        std::ifstream f(filename, std::ios::binary);
+
+        if (!f.is_open()) {
+            std::cerr << "Error: Unable to open file." << std::endl;
+            return -1;
+        }
+
+        f.seekg(0, std::ios::end);
+
+        long file_size = f.tellg();
+        double f_size = 0.0;
+        if (unit == "MB") {
+            f_size = file_size / ((double)1024 * 1024);  // converting into mb
+        } else if (unit == "KB") {
+            f_size = file_size / ((double)1024);
+        }
+
+        double speed = f_size / ((double)(end - start) / CLOCKS_PER_SEC);
+        return speed;
     }
 };
