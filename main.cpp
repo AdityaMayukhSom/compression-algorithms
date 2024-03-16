647f788453ff3bc52f78fc5eb527154cc86ccd8e
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-16 15:02:12 +0530

    added timing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "./src/algorithms/lz77.cpp"
+#include "./src/algorithms/lz78.cpp"
 #include "./src/algorithms/lzw.cpp"
 #include "./src/algorithms/run_length_encoding.cpp"
 #include "./src/utils/setup_utils.cpp"
@@ -26,16 +28,30 @@ int main(int argc, char* argv[]) {
     SetupUtils::createDirectory("compressed");
     SetupUtils::createDirectory("decompressed");
 
-    std::string input_file_path = "./original/" + default_test_file_name;
-    std::string compressed_file_path = "./compressed/" + tokens[0] + "_compressed.dev";
-    std::string decompressed_file_path = "./decompressed/" + tokens[0] + "_decompressed." + tokens[1];
+    std::vector<Compressor*> comps = {
+        new RunLengthEncodingCompressor(),  //
+        new LZWCompressor(),                //
+        new LZ77Compressor(),               //
+        new LZ78Compressor()                //
+    };
 
-    Compressor* rlec = new RunLengthEncodingCompressor();
-    Compressor* lzwc = new LZWCompressor();
-    Compressor* lz77c = new LZ77Compressor();
+    for (Compressor* com : comps) {
+        std::cout << ">> Using " << com->algorithm_name << " Algorithm"
+                  << "\n\n";
+        std::string input_file_path = "./original/" + default_test_file_name;
+        std::string compressed_file_path = "./compressed/" + tokens[0] + "_compressed." + com->compressor_ext;
+        std::string decompressed_file_path = "./decompressed/" + tokens[0] + "_decompressed." + tokens[1];
 
-    Compressor* com = lz77c;
+        clock_t comp_start = clock();
+        com->compress(input_file_path, compressed_file_path);
+        clock_t comp_end = clock();
 
-    com->compress(input_file_path, compressed_file_path);
-    com->decompress(compressed_file_path, decompressed_file_path);
+        printf("Compression Time: %f [Seconds]\n\n", ((double)(comp_end - comp_start) / CLOCKS_PER_SEC));
+
+        clock_t decomp_start = clock();
+        com->decompress(compressed_file_path, decompressed_file_path);
+        clock_t decomp_end = clock();
+
+        printf("Deompression Time: %f [Seconds]\n\n", ((double)(decomp_end - decomp_start) / CLOCKS_PER_SEC));
+    }
 }
