eea03070ac4615c7b69f563a89773eda14239fb8
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-17 06:32:43 +0530

    handled edge cases in space savings

--- src/profilers/profiler.cpp
+++ src/profilers/profiler.cpp
@@ -5,21 +5,27 @@
 class Profiler {
    public:
     static double calculate_space_saving(double compression_ratio) {
+        if (compression_ratio < 0) {
+            return -1;
+        }
+        if (compression_ratio == 0) {
+            return 100;
+        }
         return (1 - (1 / compression_ratio)) * 100;
     }
-    static double calculate_compression_ratio(
-        const std::string original_file_path,
-        const std::string compressed_file_path) {
+
+    static double calculate_compression_ratio(const std::string original_file_path,
+                                              const std::string compressed_file_path) {
         std::ifstream original_file(original_file_path, std::ios::binary);
         std::ifstream compressed_file(compressed_file_path, std::ios::binary);
 
         if (!original_file.is_open()) {
-            std::cerr << "Error: Unable to open original file." << std::endl;
+            std::cerr << "Error: Unable to open original file: " << original_file_path << std::endl;
             return -1;
         }
 
         if (!compressed_file.is_open()) {
-            std::cerr << "Error: Unable to open compressed file." << std::endl;
+            std::cerr << "Error: Unable to open compressed file:" << compressed_file_path << std::endl;
             return -1;
         }
         original_file.seekg(0, std::ios::end);
@@ -32,17 +38,14 @@ class Profiler {
             return -1;
         }
 
-        double compression_ratio =
-            original_file_size / (double)compressed_file_size;
+        double compression_ratio = original_file_size / (double)compressed_file_size;
 
         original_file.close();
         compressed_file.close();
         return compression_ratio;
     }
 
-    static double calculate_processing_speed(const clock_t start,
-                                             const clock_t end,
-                                             const std::string filename,
+    static double calculate_processing_speed(const clock_t start, const clock_t end, const std::string filename,
                                              const std::string unit = "MB") {
         std::ifstream f(filename, std::ios::binary);
 
