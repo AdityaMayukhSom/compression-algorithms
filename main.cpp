70604d9c8cd9688a6f414cae9dba34a7413f1ddd
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-03-17 12:33:44 +0530

    fixed non existent file error

--- main.cpp
+++ main.cpp
@@ -11,22 +11,20 @@
 #include "./src/lib/mem_monitor.hpp"
 #include "./src/lib/variadic_table.hpp"
 #include "./src/profilers/profiler.cpp"
+#include "./src/utils/file_size_util.cpp"
 #include "./src/utils/setup_utils.cpp"
-#include "./src/utils/size_convertor.cpp"
 #include "./src/utils/tokenizer.cpp"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "USE: <executable_name> <filename1>.<extension> "
-                     "<filename2>.<extension>..."
-                  << std::endl;
-        std::cerr << "NOTE: files must reside inside ./original directory"
-                  << std::endl;
+        std::cerr << "USE: <executable_name> <filename1>.<extension> <filename2>.<extension>..." << std::endl;
+        std::cerr << "NOTE: files must reside inside ./original directory" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    std::vector<std::string> test_files = {
-        "test1.txt", "test2.txt", "test3.txt", "test4.txt", "test5.txt"};
+    std::vector<std::string> test_files = {"test1.txt", "test2.txt", "test3.txt", "test4.txt", "test5.txt"};
+
+    test_files.clear();
     for (int i = 1; i < argc; i++) {
         test_files.push_back(argv[i]);
     }
@@ -47,50 +45,49 @@ int main(int argc, char* argv[]) {
     mm.event("Algorithms Execution Started");
     for (Compressor* com : comps) {
         std::cout << com->algorithm_name << "\n";
-        VariadicTable<std::string, double, double, double, double, double,
-                      double>
-            table({"File", "Compression Time (Sec)", "Compression Ratio",
-                   "Compression Speed (Mbps)", "Space Saving (%)",
-                   "Decompression Time (Sec)", "Decompression Speed (Mbps)"});
+        VariadicTable<std::string, double, double, double, double, double, double> table(
+            {"File", "Compression Time (Sec)", "Compression Ratio", "Compression Speed (Mbps)", "Space Saving (%)",
+             "Decompression Time (Sec)", "Decompression Speed (Mbps)"});
 
         for (std::string test_file : test_files) {
-            std::vector<std::string> tokens =
-                Tokenizer::getTokens(test_file, ".");
+            std::vector<std::string> tokens = Tokenizer::getTokens(test_file, ".");
 
-            std::cout << ">> Using " << com->algorithm_name << " Algorithm For "
-                      << test_file << "\n";
+            // std::cout << ">> executing " << com->algorithm_name << " algorithm on " << test_file << "\n";
+
             std::string input_file_path = "./original/" + test_file;
-            std::string compressed_file_path = "./compressed/" + tokens[0] +
-                                               "_compressed." +
-                                               com->compressor_ext;
-            std::string decompressed_file_path =
-                "./decompressed/" + tokens[0] + "_decompressed." + tokens[1];
+            std::string compressed_file_path = "./compressed/" + tokens[0] + "_compressed." + com->compressor_ext;
+            std::string decompressed_file_path = "./decompressed/" + tokens[0] + "_decompressed." + tokens[1];
+
+            if (!SetupUtils::exists(input_file_path)) {
+                std::cerr << "no such file exists: [" << input_file_path << "]\n";
+                std::cerr << "cannot compress non existent file, ignoring\n";
+                continue;
+            }
 
             mm.event("started " + com->algorithm_name + " compression");
+
             clock_t comp_start = clock();
             com->compress(input_file_path, compressed_file_path);
             clock_t comp_end = clock();
 
-            double compression_ratio = Profiler::calculate_compression_ratio(
-                input_file_path, compressed_file_path);
-
             mm.event("started " + com->algorithm_name + " decompression");
+
             clock_t decomp_start = clock();
             com->decompress(compressed_file_path, decompressed_file_path);
             clock_t decomp_end = clock();
 
-            table.addRow(test_file + "(" +
-                             SizeConvertor::getFileSize(input_file_path) + ")",
-                         ((double)(comp_end - comp_start) / CLOCKS_PER_SEC),
-                         compression_ratio,
-                         Profiler::calculate_processing_speed(
-                             comp_start, comp_end, input_file_path),
+            double compression_ratio = Profiler::calculate_compression_ratio(input_file_path, compressed_file_path);
+
+            table.addRow(test_file + "(" + FileSizeUtil::getFileSize(input_file_path) + ")",
+                         ((double)(comp_end - comp_start) / CLOCKS_PER_SEC), compression_ratio,
+                         Profiler::calculate_processing_speed(comp_start, comp_end, input_file_path),
                          Profiler::calculate_space_saving(compression_ratio),
                          ((double)(decomp_end - decomp_start) / CLOCKS_PER_SEC),
-                         Profiler::calculate_processing_speed(
-                             decomp_start, decomp_end, compressed_file_path));
+                         Profiler::calculate_processing_speed(decomp_start, decomp_end, decompressed_file_path));
         }
+
         table.print(std::cout);
     }
+
     mm.event("Algorithms Execution Finished");
 }
